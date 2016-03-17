00be3250ae0b990e0abe3a37ce2e54cf33221d75
pese - Sergey Penkovsky <pese@gelios.ru>
2016-03-17 11:00:03 +0300

    reform code

--- apps/erlymon/src/em_http/handlers/v1/em_http_api_users_v1_handler.erl
+++ apps/erlymon/src/em_http/handlers/v1/em_http_api_users_v1_handler.erl
@@ -32,94 +32,79 @@
 
 %% GET http://demo.traccar.org/api/user/get?_dc=1436251203853&page=1&start=0&limit=25
 %% {"success":true,"data":[]}
--spec init(Req::cowboy_req:req(), Opts::any()) -> {ok, cowboy_req:req(), any()}.
+-spec init(Req :: cowboy_req:req(), Opts :: any()) -> {ok, cowboy_req:req(), any()}.
 init(Req, Opts) ->
-  Method = cowboy_req:method(Req),
-      {ok, request(Method, Req), Opts}.
+  case cowboy_session:get(user, Req) of
+    {undefined, Req2} ->
+      {ok, cowboy_req:reply(?STATUS_NOT_FOUND, Req2), Opts};
+    {User, Req2} ->
+      Method = cowboy_req:method(Req2),
+      {ok, request(Method, Req2, User), Opts}
+  end.
 
--spec request(Method::binary(), Opts::any()) -> cowboy_req:req().
-request(?GET, Req) ->
-    get_users(Req);
-request(?POST, Req) ->
-    add_user(Req);
-request(?PUT, Req) ->
-    update_user(Req);
-request(?DELETE, Req) ->
-    remove_user(Req);
-request(_, Req) ->
+-spec request(Method :: binary(), Req :: cowboy_req:req(), User :: map()) -> cowboy_req:req().
+request(?GET, Req, User) ->
+  get_users(Req, User);
+request(?POST, Req, User) ->
+  add_user(Req, User);
+request(?PUT, Req, User) ->
+  update_user(Req, User);
+request(?DELETE, Req, User) ->
+  remove_user(Req, User);
+request(_, Req, _) ->
   %% Method not allowed.
   cowboy_req:reply(?STATUS_METHOD_NOT_ALLOWED, [], <<"Allowed GET,POST,PUT,DELETE requests.">>, Req).
 
--spec get_users(Req::cowboy_req:req()) -> cowboy_req:req().
-get_users(Req) ->
-    case cowboy_session:get(user, Req) of
-        {undefined, Req2} ->
-            cowboy_req:reply(?STATUS_NOT_FOUND, Req2);
-        {User, Req2} ->
-            em_logger:info("User: ~w", [maps:get(<<"id">>,User)]),
-            case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
-                false ->
-                    cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req2);
-                _ ->
-                    Users = em_data_manager:get_users(),
-                    cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(Users), Req2)
-            end
-    end.
+-spec get_users(Req :: cowboy_req:req(), User :: map()) -> cowboy_req:req().
+get_users(Req, User) ->
+  em_logger:info("User: ~w", [maps:get(<<"id">>, User)]),
+  case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req);
+    _ ->
+      Users = em_data_manager:get_users(),
+      cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(Users), Req)
+  end.
 
--spec add_user(Req::cowboy_req:req()) -> cowboy_req:req().
-add_user(Req) ->
-    case cowboy_session:get(user, Req) of
-        {undefined, Req2} ->
-            cowboy_req:reply(?STATUS_NOT_FOUND, Req2);
-        {User, Req2} ->
-            {ok, [{JsonBin, true}], Req3} = cowboy_req:body_qs(Req2),
-            UserModel = em_json:decode(JsonBin),
-            case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
-                false ->
-                    cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req2);
-                _ ->
-                    NewUser = em_data_manager:create_user(UserModel),
-                    cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(NewUser), Req3)
-            end
-    end.
-    
--spec remove_user(Req :: cowboy_req:req()) -> cowboy_req:req().
-remove_user(Req) ->
-    case cowboy_session:get(user, Req) of
-        {undefined, Req2} ->
-            cowboy_req:reply(?STATUS_NOT_FOUND, Req2);
-        {User, Req2} ->
-            case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
-                false ->
-                    cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req2);
-                _ ->
-                    {ok, [{JsonBin, true}], Req3} = cowboy_req:body_qs(Req2),
-                    UserModel = em_json:decode(JsonBin),
-                    em_data_manager:delete_user(UserModel),
-                    cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(UserModel), Req3)
-            end
-    end.
-    
--spec update_user(Req::cowboy_req:req()) -> cowboy_req:req().
-update_user(Req) ->
-    case cowboy_session:get(user, Req) of
-        {undefined, Req2} ->
-            cowboy_req:reply(?STATUS_NOT_FOUND, Req2);
-        {User, Req2} ->
-            {ok, [{JsonBin, true}], Req3} = cowboy_req:body_qs(Req2),
-            UserModel = em_json:decode(JsonBin),
-            case check_permission(maps:get(<<"id">>, User), maps:get(<<"id">>, UserModel)) of
-                false ->
-                    cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req3);
-                _ ->
-                    em_data_manager:update_user(maps:remove(<<"o">>,UserModel)),
-                    cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(UserModel), Req3)
-            end
-    end.
+-spec add_user(Req :: cowboy_req:req(), User :: map()) -> cowboy_req:req().
+add_user(Req, User) ->
+  {ok, [{JsonBin, true}], Req2} = cowboy_req:body_qs(Req),
+  UserModel = em_json:decode(JsonBin),
+  case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req2);
+    _ ->
+      NewUser = em_data_manager:create_user(UserModel),
+      cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(NewUser), Req2)
+  end.
+
+-spec remove_user(Req :: cowboy_req:req(), User :: map()) -> cowboy_req:req().
+remove_user(Req, User) ->
+  case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req);
+    _ ->
+      {ok, [{JsonBin, true}], Req2} = cowboy_req:body_qs(Req),
+      UserModel = em_json:decode(JsonBin),
+      em_data_manager:delete_user(UserModel),
+      cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(UserModel), Req2)
+  end.
+
+-spec update_user(Req :: cowboy_req:req(), User :: map()) -> cowboy_req:req().
+update_user(Req, User) ->
+  {ok, [{JsonBin, true}], Req2} = cowboy_req:body_qs(Req),
+  UserModel = em_json:decode(JsonBin),
+  case check_permission(maps:get(<<"id">>, User), maps:get(<<"id">>, UserModel)) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req2);
+    _ ->
+      em_data_manager:update_user(maps:remove(<<"o">>, UserModel)),
+      cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(UserModel), Req2)
+  end.
 
 
 
 check_permission(CurrUserId, UpdateUserId) when CurrUserId == UpdateUserId ->
-    true;
+  true;
 check_permission(CurrUserId, _) ->
-    em_permissions_manager:check_admin(CurrUserId).
+  em_permissions_manager:check_admin(CurrUserId).
