d543f1cd1fd5b41c7f4613b5c66c95b1cb898991
pese - Sergey Penkovsky <pese@gelios.ru>
2016-03-17 15:56:54 +0300

    implemented logic ws

--- apps/erlymon/src/em_http/handlers/v1/em_http_api_socket_v1_handler.erl
+++ apps/erlymon/src/em_http/handlers/v1/em_http_api_socket_v1_handler.erl
@@ -31,17 +31,40 @@
 -export([websocket_handle/3]).
 -export([websocket_info/3]).
 
+
+%% http://blog.dberg.org/2012/04/using-gproc-and-cowboy-to-pass-messages.html
+%% http://ninenines.eu/docs/en/cowboy/HEAD/guide/ws_handlers/
+
 init(Req, Opts) ->
-	erlang:start_timer(1000, self(), <<"Hello!">>),
-	{cowboy_websocket, Req, Opts}.
+  case cowboy_session:get(user, Req) of
+    {undefined, Req2} ->
+      {cowboy_websocket, Req2, Opts};
+    {User, Req2} ->
+      erlang:start_timer(1000, self(), User),
+      {cowboy_websocket, Req2, Opts}
+  end.
 
 websocket_handle({text, Msg}, Req, State) ->
-	{reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
+  {reply, {text, <<"That's what she said! ", Msg/binary>>}, Req, State};
 websocket_handle(_Data, Req, State) ->
-	{ok, Req, State}.
+  {ok, Req, State}.
 
-websocket_info({timeout, _Ref, Msg}, Req, State) ->
-	erlang:start_timer(1000, self(), <<"How' you doin'?">>),
-	{reply, {text, Msg}, Req, State};
+websocket_info({timeout, _Ref, User}, Req, State) ->
+  Devices = em_data_manager:get_devices(maps:get(<<"id">>, User), #{<<"_id">> => false, <<"password">> => false, <<"lastUpdate">> => false}),
+  LastMessages = lists:foldl(fun(Device, Acc) ->
+    case em_data_manager:get_last_message(maps:get(<<"positionId">>, Device), maps:get(<<"id">>, Device)) of
+      null ->
+        Acc;
+      Message ->
+        [Message | Acc]
+    end
+                             end, [], Devices),
+  erlang:start_timer(1000, self(), User),
+
+  DataModel = #{
+    <<"devices">> => Devices,
+    <<"positions">> => LastMessages
+  },
+  {reply, {text, em_json:encode(DataModel)}, Req, State};
 websocket_info(_Info, Req, State) ->
-	{ok, Req, State}.
+  {ok, Req, State}.
