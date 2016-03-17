b93a8d176c07e450a21a1691781aafdc125f3f29
Sergey A. Penkovsky <sergey.penkovsky@gmail.com>
2016-03-17 06:47:32 +0300

    implemented permission requests

--- apps/erlymon/src/em_http/handlers/v1/em_http_api_permissions_v1_handler.erl
+++ apps/erlymon/src/em_http/handlers/v1/em_http_api_permissions_v1_handler.erl
@@ -32,37 +32,45 @@
 
 -spec init(Req::cowboy_req:req(), Opts::any()) -> {ok, cowboy_req:req(), any()}.
 init(Req, Opts) ->
-    case cowboy_session:get(user, Req) of
-        {undefined, Req2} ->
-            {ok, cowboy_req:reply(?STATUS_NOT_FOUND, Req2), Opts};
-        {User, Req2} ->
-            Method = cowboy_req:method(Req),
-            {ok, request(Method, Req, User), Opts}
-    end.
+  case cowboy_session:get(user, Req) of
+    {undefined, Req2} ->
+      {ok, cowboy_req:reply(?STATUS_NOT_FOUND, Req2), Opts};
+    {User, Req2} ->
+      Method = cowboy_req:method(Req2),
+      {ok, request(Method, Req2, User), Opts}
+  end.
 
 -spec request(Method::binary(), Req::cowboy_req:req(), User::map()) -> cowboy_req:req().
 request(?POST, Req, User) ->
-    add_permission(Req, User);    
+  add_permission(Req, User);
 request(?DELETE, Req, User) ->
-    remove_permission(Req, User);
+  remove_permission(Req, User);
 request(_, Req, _) ->
   %% Method not allowed.
   cowboy_req:reply(?STATUS_METHOD_NOT_ALLOWED, Req).
 
--spec add_permission(Req::cowboy_req:req(), User::map()) -> cowboy_req:req().    
+-spec add_permission(Req::cowboy_req:req(), User::map()) -> cowboy_req:req().
 add_permission(Req, User) ->
-    case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
-        false ->
-              cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req);
-        _ ->
-              cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(em_data_manager:get_all_devices()), Req)
-    end.
-    
--spec remove_permission(Req::cowboy_req:req(), User::map()) -> cowboy_req:req().    
+  case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req);
+    _ ->
+      {ok, [{JsonBin, true}], Req2} = cowboy_req:body_qs(Req),
+      %% {userId: 1458137425, deviceId: 1458137433}
+      PermissionModel = em_json:decode(JsonBin),
+      em_data_manager:link_device(maps:get(<<"userId">>, PermissionModel), maps:get(<<"deviceId">>, PermissionModel)),
+      cowboy_req:reply(?STATUS_OK, Req2)
+  end.
+
+-spec remove_permission(Req::cowboy_req:req(), User::map()) -> cowboy_req:req().
 remove_permission(Req, User) ->
-    case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
-        false ->
-              cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req);
-        _ ->
-              cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(em_data_manager:get_all_devices()), Req)
-    end.
+  case em_permissions_manager:check_admin(maps:get(<<"id">>, User)) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Admin access required">>, Req);
+    _ ->
+      {ok, [{JsonBin, true}], Req2} = cowboy_req:body_qs(Req),
+      %% {userId: 1458137425, deviceId: 1458137433}
+      PermissionModel = em_json:decode(JsonBin),
+      em_data_manager:unlink_device(maps:get(<<"userId">>, PermissionModel), maps:get(<<"deviceId">>, PermissionModel)),
+      cowboy_req:reply(?STATUS_OK, Req2)
+  end.
