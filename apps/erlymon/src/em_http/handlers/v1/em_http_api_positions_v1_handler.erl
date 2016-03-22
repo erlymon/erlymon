d519a671b06d919eee1361f6937bfc89b25374f5
Sergey A. Penkovsky <sergey.penkovsky@gmail.com>
2016-03-22 06:18:05 +0300

    refactored code

--- apps/erlymon/src/em_http/handlers/v1/em_http_api_positions_v1_handler.erl
+++ apps/erlymon/src/em_http/handlers/v1/em_http_api_positions_v1_handler.erl
@@ -33,41 +33,38 @@
 
 %% GET http://demo.traccar.org/api/user/get?_dc=1436251203853&page=1&start=0&limit=25
 %% {"success":true,"data":[]}
--spec init(Req::cowboy_req:req(), Opts::any()) -> {ok, cowboy_req:req(), any()}.
+-spec init(Req :: cowboy_req:req(), Opts :: any()) -> {ok, cowboy_req:req(), any()}.
 init(Req, Opts) ->
-  Method = cowboy_req:method(Req),
-      {ok, request(Method, Req), Opts}.
+  case cowboy_session:get(user, Req) of
+    {undefined, Req2} ->
+      {ok, cowboy_req:reply(?STATUS_NOT_FOUND, Req2), Opts};
+    {User, Req2} ->
+      Method = cowboy_req:method(Req2),
+      {ok, request(Method, Req2, User), Opts}
+  end.
 
--spec request(Method::binary(), Opts::any()) -> cowboy_req:req().
-request(?GET, Req) ->
-    get_positions(Req);
-request(_, Req) ->
+-spec request(Method :: binary(), Opts :: any(), User :: map()) -> cowboy_req:req().
+request(?GET, Req, User) ->
+  get_positions(Req, User);
+request(_, Req, _) ->
   %% Method not allowed.
   cowboy_req:reply(?STATUS_METHOD_NOT_ALLOWED, Req).
 
--spec get_positions(Req::cowboy_req:req()) -> cowboy_req:req().
-get_positions(Req) ->
-    case cowboy_session:get(user, Req) of
-        {undefined, Req2} ->
-            cowboy_req:reply(?STATUS_NOT_FOUND, Req2);
-        {User, Req2} ->
-            %%#{deviceId := DeviceId, from := From, to := To} = cowboy_req:match_qs([deviceId, from, to], Req),
-            Qs = cowboy_req:parse_qs(Req2),
-            DeviceId = list_to_integer(binary_to_list(proplists:get_value(<<"deviceId">>, Qs, <<"0">>))),
-            From = proplists:get_value(<<"from">>, Qs, <<"">>),
-            To = proplists:get_value(<<"to">>, Qs, <<"">>),
-            em_logger:info("Device ID: ~w, From: ~s, To: ~s", [DeviceId, From, To]),
-            %%Id = str_to_int(DeviceId),
-            case em_permissions_manager:check_device(maps:get(<<"id">>, User), DeviceId) of
-                false ->
-                    cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Device access denied">>, Req2);
-                _ ->
-                    {ok, TimeFrom} = em_helper_time:parse(<<"%Y-%m-%dT%H:%M:%S.000Z">>, From),
-                    {ok, TimeTo} = em_helper_time:parse(<<"%Y-%m-%dT%H:%M:%S.000Z">>, To),
-                    Messages = em_data_manager:get_messages(DeviceId, TimeFrom * 1000, TimeTo * 1000),
-                    cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(Messages), Req2)
-            end
-    end.
-
-str_to_int(Str) ->
-    list_to_integer(binary_to_list(Str)).
+-spec get_positions(Req :: cowboy_req:req(), User :: map()) -> cowboy_req:req().
+get_positions(Req, User) ->
+  %%#{deviceId := DeviceId, from := From, to := To} = cowboy_req:match_qs([deviceId, from, to], Req),
+  Qs = cowboy_req:parse_qs(Req),
+  DeviceId = list_to_integer(binary_to_list(proplists:get_value(<<"deviceId">>, Qs, <<"0">>))),
+  From = proplists:get_value(<<"from">>, Qs, <<"">>),
+  To = proplists:get_value(<<"to">>, Qs, <<"">>),
+  em_logger:info("Device ID: ~w, From: ~s, To: ~s", [DeviceId, From, To]),
+  %%Id = str_to_int(DeviceId),
+  case em_permissions_manager:check_device(maps:get(<<"id">>, User), DeviceId) of
+    false ->
+      cowboy_req:reply(?STATUS_FORBIDDEN, [], <<"Device access denied">>, Req);
+    _ ->
+      {ok, TimeFrom} = em_helper_time:parse(<<"%Y-%m-%dT%H:%M:%S.000Z">>, From),
+      {ok, TimeTo} = em_helper_time:parse(<<"%Y-%m-%dT%H:%M:%S.000Z">>, To),
+      Messages = em_data_manager:get_messages(DeviceId, TimeFrom * 1000, TimeTo * 1000),
+      cowboy_req:reply(?STATUS_OK, ?HEADERS, em_json:encode(Messages), Req)
+  end.
