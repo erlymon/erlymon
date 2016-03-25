762bf7102285fe3d3d8d5d73bd86b1d49e724a66
Sergey A. Penkovsky <sergey.penkovsky@gmail.com>
2016-03-26 00:27:10 +0300

    added notify

--- apps/erlymon/src/em_http/handlers/v1/em_http_api_socket_v1_handler.erl
+++ apps/erlymon/src/em_http/handlers/v1/em_http_api_socket_v1_handler.erl
@@ -30,41 +30,45 @@
 -export([init/2]).
 -export([websocket_handle/3]).
 -export([websocket_info/3]).
+-export([notify/3]).
 
 
 %% http://blog.dberg.org/2012/04/using-gproc-and-cowboy-to-pass-messages.html
 %% http://ninenines.eu/docs/en/cowboy/HEAD/guide/ws_handlers/
 
+notify(UserId, EventType, EventData) ->
+    em_helper_process:send({p, l, {user, UserId}}, {EventType, EventData}).
+
 init(Req, Opts) ->
-  case cowboy_session:get(user, Req) of
-    {undefined, Req2} ->
-      {cowboy_websocket, Req2, Opts};
-    {User, Req2} ->
-      erlang:start_timer(1000, self(), User),
-      {cowboy_websocket, Req2, Opts}
-  end.
+    case cowboy_session:get(user, Req) of
+        {undefined, Req2} ->
+            {cowboy_websocket, Req2, Opts};
+        {User, Req2} ->
+            UserId = maps:get(<<"id">>, User),
+            em_logger:info("Process reg for user: ~w", [UserId]),
+            em_helper_process:reg({p, l, {user, UserId}}),
+            Devices = em_data_manager:get_devices(maps:get(<<"id">>, User)),
+            GetLastPosition = fun(Device, Acc) ->
+                                      case em_data_manager:get_last_message(maps:get(<<"positionId">>, Device), maps:get(<<"id">>, Device)) of
+                                          null ->
+                                              Acc;
+                                          Message ->
+                                              [Message | Acc]
+                                      end
+                              end,
+            Positions = lists:foldl(GetLastPosition, [], Devices),
+            notify(UserId, init, {Devices, Positions}),
+            {cowboy_websocket, Req2, Opts}
+    end.
 
-websocket_handle({text, Msg}, Req, State) ->
-  {reply, {text, <<"That's what she said! ", Msg/binary>>}, Req, State};
 websocket_handle(_Data, Req, State) ->
-  {ok, Req, State}.
+    {ok, Req, State}.
 
-websocket_info({timeout, _Ref, User}, Req, State) ->
-  Devices = em_data_manager:get_devices(maps:get(<<"id">>, User), #{<<"_id">> => false, <<"password">> => false}),
-  LastMessages = lists:foldl(fun(Device, Acc) ->
-    case em_data_manager:get_last_message(maps:get(<<"positionId">>, Device), maps:get(<<"id">>, Device)) of
-      null ->
-        Acc;
-      Message ->
-        [Message | Acc]
-    end
-                             end, [], Devices),
-  erlang:start_timer(1000, self(), User),
-
-  DataModel = #{
-    <<"devices">> => Devices,
-    <<"positions">> => LastMessages
-  },
-  {reply, {text, em_json:encode(DataModel)}, Req, State};
+websocket_info({init, {Devices, Positions}}, Req, State) ->
+    {reply, {text, em_json:encode(#{<<"devices">> => Devices, <<"positions">> => Positions})}, Req, State};
+websocket_info({devices, Devices}, Req, State) ->
+    {reply, {text, em_json:encode(#{<<"devices">> => Devices})}, Req, State};
+websocket_info({positions, Positions}, Req, State) ->
+    {reply, {text, em_json:encode(#{<<"positions">> => Positions})}, Req, State};
 websocket_info(_Info, Req, State) ->
-  {ok, Req, State}.
+    {ok, Req, State}.
