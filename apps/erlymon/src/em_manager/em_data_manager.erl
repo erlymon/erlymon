1ee93a69446cabf1a3c2dda6468a8833f6f50da3
erlymon <dev@erlymon.org>
2016-08-31 06:24:23 +0300

    refactored code

--- apps/erlymon/src/em_manager/em_data_manager.erl
+++ apps/erlymon/src/em_manager/em_data_manager.erl
@@ -28,10 +28,6 @@
 
 %% API
 -export([
-  init/0
-]).
-
--export([
   get_server/0,
   update_server/1
 ]).
@@ -39,7 +35,6 @@
 -export([
   create_user/1,
   update_user/1,
-  update_user/3,
   delete_user/1,
   check_user/2,
   get_users/0
@@ -49,11 +44,8 @@
   link_device/1,
   unlink_device/1,
   create_device/1,
-  create_device/3,
   update_device/1,
-  update_device/4,
   delete_device/1,
-  delete_device/2,
   get_devices/1,
   get_device_by_uid/1,
   get_all_devices/0
@@ -64,31 +56,6 @@
   get_positions/3,
   get_last_position/2
 ]).
-
-
-
-init() ->
-    em_storage:ensure_index(users, #{<<"key">> => #{<<"id">> => 1}, <<"name">> => <<"id_1">>, <<"unique">> => true}),
-    em_storage:ensure_index(users, #{<<"key">> => #{<<"email">> => 1}, <<"name">> => <<"email_1">>, <<"unique">> => true}),
-
-    em_storage:ensure_index(devices, #{<<"key">> => #{<<"id">> => 1}, <<"name">> => <<"id_1">>, <<"unique">> => true}),
-    em_storage:ensure_index(devices, #{<<"key">> => #{<<"uniqueId">> => 1}, <<"name">> => <<"uniqueId_1">>, <<"unique">> => true}),
-
-    em_storage:ensure_index(servers, #{<<"key">> => #{<<"id">> => 1}, <<"name">> => <<"id_1">>, <<"unique">> => true}),
-
-    em_storage:ensure_index(messages, #{<<"key">> => #{<<"id">> => 1}, <<"name">> => <<"id_1">>, <<"unique">> => true}),
-    em_storage:ensure_index(messages, #{<<"key">> => #{<<"deviceId">> => 1, <<"fixTime">> => 1, <<"imei">> => 1}, <<"name">> => <<"deviceId_1_fixTime_1_imei_1">>, <<"unique">> => true}),
-    case em_storage_server:get() of
-        null ->
-            em_storage_server:create(true, 0, 0, 0),
-            Admin = em_storage_user:create(<<"admin">>, <<"admin">>, <<"admin">>, true),
-            Device = em_storage_device:create(<<"test1">>, <<"123456789012345">>),
-            %%link_device(maps:get(<<"id">>, Admin), maps:get(<<"id">>, Device)),
-            em_logger:info("Create account admin: username: ~s, password: ~s", [maps:get(<<"email">>, Admin), maps:get(<<"password">>, Admin)]);
-        _ ->
-            em_logger:info("Storage already init")
-    end.
-
 
 get_server() ->
   em_storage:get_server().
@@ -114,41 +81,11 @@ create_position(DeviceModel, PositionModel) ->
           Reason
       end.
 
-create_message(DeviceId, Protocol, MessageParams) ->
-    case em_storage_message:get(DeviceId, maps:get(<<"deviceTime">>, MessageParams)) of
-      #{} ->
-        case em_storage_message:create(DeviceId, Protocol, MessageParams) of
-          null ->
-            null;
-          Message ->
-            em_storage_device:update(DeviceId, #{
-              <<"positionId">> => maps:get(<<"id">>, Message)
-            }),
-            Device = em_storage_device:get_by_id(DeviceId),
-            em_manager_event:broadcast(convert_date_in_device(Device), convert_date_in_message(maps:remove(<<"_id">>, Message))),
-            Message
-        end;
-      _ ->
-        null
-    end.
-
 get_positions(DeviceId, TimeFrom, TimeTo) ->
     em_storage:get_positions(DeviceId, TimeFrom, TimeTo).
 
 get_last_position(PositionId, DeviceId) ->
     em_storage:get_last_position(PositionId,  DeviceId).
-
-
-convert_date_in_message(Message) ->
-  %% deviceTime: "2016-01-09T14:56:18.000+0000"
-  %% fixTime: "2016-01-09T14:56:18.000+0000"
-  %% serverTime: "2016-01-09T14:57:16.000+0000"
-  {ok, DeviceTime} = em_helper_time:format(<<"%Y-%m-%dT%H:%M:%S.000%z">>, maps:get(<<"deviceTime">>, Message)),
-  {ok, FixTime} = em_helper_time:format(<<"%Y-%m-%dT%H:%M:%S.000%z">>, maps:get(<<"fixTime">>, Message)),
-  {ok, ServerTime} = em_helper_time:format(<<"%Y-%m-%dT%H:%M:%S.000%z">>, maps:get(<<"serverTime">>, Message)),
-  NewMessage0 = maps:put(<<"deviceTime">>, DeviceTime, Message),
-  NewMessage1 = maps:put(<<"fixTime">>, FixTime, NewMessage0),
-  maps:put(<<"serverTime">>, ServerTime, NewMessage1).
 
 create_user(User) ->
     case em_storage:get_user_by_email(User#user.email) of
@@ -161,17 +98,12 @@ create_user(User) ->
 update_user(User) ->
   em_storage:update_user(User).
 
-
-
-update_user(UserId, Field, Value) ->
-    em_storage_user:update(UserId, Field, Value).
-
 delete_user(User) ->
     em_storage:delete_user(User).
 
 check_user(Email, Password) ->
-  CheckPass = fun(Password, User = #user{hashPassword = HashPassword}) ->
-                case Password =:= HashPassword of
+  CheckPass = fun(CurrPassword, User = #user{hashPassword = HashPassword}) ->
+                case CurrPassword =:= HashPassword of
                   true -> {ok, User};
                   false -> {error, <<"Access is denied">>}
                 end
@@ -184,45 +116,14 @@ check_user(Email, Password) ->
 get_users() ->
     em_storage:get_users().
 
-
-
 create_device(Device) ->
     em_storage:create_device(Device).
-
-create_device(UserId, DeviceName, DeviceUniqueId) ->
-    case em_storage_device:create(DeviceName, DeviceUniqueId) of
-        null ->
-            null;
-        Device = #{<<"id">> := DeviceId} ->
-            em_storage_permission:create(UserId, DeviceId),
-            Device
-    end.
 
 update_device(Device) ->
     em_storage:update_device(Device).
 
-update_device(UserId, DeviceId, Field, Value) ->
-    case em_storage_permission:get(UserId, DeviceId) of
-        null ->
-            false;
-        _ ->
-            em_storage_device:update(DeviceId, Field, Value),
-            true
-    end.
-
 delete_device(Device) ->
     em_storage:delete_device(Device).
-
-delete_device(UserId, DeviceId) ->
-    case em_storage_permission:get(UserId, DeviceId) of
-        null ->
-            false;
-        _ ->
-            em_storage_device:delete(DeviceId),
-            em_storage_permission:delete(DeviceId),
-            true
-    end.
-
 
 get_devices(UserId) ->
   Callback = fun(Permission = #permission{deviceId = DeviceId}, Acc) ->
@@ -238,17 +139,11 @@ get_devices(UserId) ->
   Devices = lists:foldl(Callback, [], Permissions),
   {ok, Devices}.
 
-
 get_device_by_uid(UniqueId) ->
   em_storage:get_device_by_uid(UniqueId).
 
-
 get_all_devices() ->
     em_storage:get_devices().
-
-convert_date_in_device(Device) ->
-  {ok, Date} = em_helper_time:format(<<"%Y-%m-%dT%H:%M:%S.000%z">>, maps:get(<<"lastUpdate">>, Device)),
-  maps:put(<<"lastUpdate">>, Date, Device).
 
 link_device(Permission) ->
     em_storage:create_permission(Permission).
