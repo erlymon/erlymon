fe9cda5a00c4d1659117d708648be78b7f0f3eca
erlymon <dev@erlymon.org>
2016-10-08 18:19:18 +0300

    implemented function check user

--- apps/erlymon/src/em_manager/em_manager_users.erl
+++ apps/erlymon/src/em_manager/em_manager_users.erl
@@ -13,21 +13,24 @@
 
 -include("em_records.hrl").
 
+-include_lib("stdlib/include/ms_transform.hrl").
+
 %% API
 -export([start_link/0]).
 
 -export([
-  get/0
-]).
+         get/0,
+         check/2
+        ]).
 
 
 %% gen_server callbacks
 -export([init/1,
-  handle_call/3,
-  handle_cast/2,
-  handle_info/2,
-  terminate/2,
-  code_change/3]).
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
 -define(SERVER, ?MODULE).
 
@@ -39,7 +42,12 @@
 
 -spec(get() -> {ok, [Rec :: #user{}]} | {error, string()}).
 get() ->
-  gen_server:call(?SERVER, {get}).
+    gen_server:call(?SERVER, {get}).
+
+
+-spec(check(Email :: string(), Password :: string()) -> {ok, Rec :: #user{}} | {error, string()}).
+check(Email, Password) ->
+    gen_server:call(?SERVER, {check, Email, Password}).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -48,9 +56,9 @@ get() ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(start_link() ->
-  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+             {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -68,15 +76,15 @@ start_link() ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(init(Args :: term()) ->
-  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term()} | ignore).
+             {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+             {stop, Reason :: term()} | ignore).
 init([]) ->
-  em_logger:info("Init users manager"),
-  Cache = ets:new(users, [set, private, {keypos, 1}]),
-  {ok, Users} = em_storage:get_users(),
-  lists:foreach(fun(User) -> ets:insert(Cache, User) end, Users),
-  em_logger:info("Loaded ~w user(s)", [length(ets:tab2list(Cache))]),
-  {ok, #state{cache = Cache}}.
+    em_logger:info("Init users manager"),
+    Cache = ets:new(users, [set, private, {keypos, 1}]),
+    {ok, Users} = em_storage:get_users(),
+    lists:foreach(fun(User) -> ets:insert(Cache, User) end, Users),
+    em_logger:info("Loaded ~w user(s)", [length(ets:tab2list(Cache))]),
+    {ok, #state{cache = Cache}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -86,17 +94,19 @@ init([]) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-    State :: #state{}) ->
-  {reply, Reply :: term(), NewState :: #state{}} |
-  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-  {stop, Reason :: term(), NewState :: #state{}}).
+                  State :: #state{}) ->
+             {reply, Reply :: term(), NewState :: #state{}} |
+             {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+             {noreply, NewState :: #state{}} |
+             {noreply, NewState :: #state{}, timeout() | hibernate} |
+             {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+             {stop, Reason :: term(), NewState :: #state{}}).
 handle_call({get}, _From, State) ->
-  do_get_users(State);
+    do_get_users(State);
+handle_call({check, Email, Password}, _From, State) ->
+    do_check_user(State, Email, Password);
 handle_call(_Request, _From, State) ->
-  {reply, ok, State}.
+    {reply, ok, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -106,11 +116,11 @@ handle_call(_Request, _From, State) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(handle_cast(Request :: term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
+             {noreply, NewState :: #state{}} |
+             {noreply, NewState :: #state{}, timeout() | hibernate} |
+             {stop, Reason :: term(), NewState :: #state{}}).
 handle_cast(_Request, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -123,11 +133,11 @@ handle_cast(_Request, State) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
+             {noreply, NewState :: #state{}} |
+             {noreply, NewState :: #state{}, timeout() | hibernate} |
+             {stop, Reason :: term(), NewState :: #state{}}).
 handle_info(_Info, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -141,10 +151,10 @@ handle_info(_Info, State) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-    State :: #state{}) -> term()).
+                State :: #state{}) -> term()).
 terminate(_Reason, #state{cache = Cache}) ->
-  ets:delete(Cache),
-  ok.
+    ets:delete(Cache),
+    ok.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -155,14 +165,30 @@ terminate(_Reason, #state{cache = Cache}) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-    Extra :: term()) ->
-  {ok, NewState :: #state{}} | {error, Reason :: term()}).
+                  Extra :: term()) ->
+             {ok, NewState :: #state{}} | {error, Reason :: term()}).
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
 do_get_users(State = #state{cache = Cache}) ->
-  {reply, ets:tab2list(Cache), State}.
+    {reply, ets:tab2list(Cache), State}.
+
+%% em_manager_users:check(<<"demo">>, <<"demo">>).
+do_check_user(State = #state{cache = Cache}, Email, Password) ->
+    CheckPass = fun(CurrPassword, User = #user{hashPassword = HashPassword}) ->
+                        case CurrPassword =:= HashPassword of
+                            true -> {ok, User};
+                            false -> {error, <<"Access is denied">>}
+                        end
+                end,
+    Match = ets:fun2ms(fun(User = #user{email = SearchEmail}) when SearchEmail =:= Email -> User end),
+    case ets:select(Cache, Match) of
+        [] ->
+            {reply, {error, <<"Access is denied">>}, State};
+        [Item|_] ->
+            {reply, CheckPass(em_password:hash(Password), Item), State}
+    end.
