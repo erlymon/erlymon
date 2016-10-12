95b3396ae063c90ea49dd47e5422dedfba27ba94
erlymon <dev@erlymon.org>
2016-10-12 19:31:27 +0300

    added header

--- apps/erlymon/src/em_manager/em_manager_users.erl
+++ apps/erlymon/src/em_manager/em_manager_users.erl
@@ -1,10 +1,25 @@
 %%%-------------------------------------------------------------------
-%%% @author sergey
-%%% @copyright (C) 2016, <COMPANY>
+%%% @author Sergey Penkovsky
+%%% @copyright (C) 2015, Sergey Penkovsky <sergey.penkovsky@gmail.com>
 %%% @doc
+%%%    Erlymon is an open source GPS tracking system for various GPS tracking devices.
 %%%
+%%%    Copyright (C) 2015, Sergey Penkovsky <sergey.penkovsky@gmail.com>.
+%%%
+%%%    This file is part of Erlymon.
+%%%
+%%%    Erlymon is free software: you can redistribute it and/or  modify
+%%%    it under the terms of the GNU Affero General Public License, version 3,
+%%%    as published by the Free Software Foundation.
+%%%
+%%%    Erlymon is distributed in the hope that it will be useful,
+%%%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%%    GNU Affero General Public License for more details.
+%%%
+%%%    You should have received a copy of the GNU Affero General Public License
+%%%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %%% @end
-%%% Created : 03. Окт. 2016 6:37 AM
 %%%-------------------------------------------------------------------
 -module(em_manager_users).
 -author("sergey").
